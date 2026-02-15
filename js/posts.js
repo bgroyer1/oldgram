cb97638a9a7bcb7d2067a1ff9f66b8374609b860
Braedan Royer <royerbraedan@gmail.com>
2026-02-15 15:41:35 -0800

    finished project

--- js/posts.js
+++ js/posts.js
@@ -1,29 +1,45 @@
+import {v4 as uuidv4} from "https://cdn.jsdelivr.net/npm/uuid@13.0.0/+esm";
+
 export const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+  {
+    uuid: uuidv4(),
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+    isLiked: false,
+    isShared: false,
+    isCommented: false,
+    
+  },
+  {
+    uuid: uuidv4(),
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+    isLiked: false,
+    isShared: false,
+    isCommented: false,
+  },
+  {
+    uuid: uuidv4(),
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+    isLiked: false,
+    isShared: false,
+    isCommented: false,
+  },
+];
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const contentFeed = document.querySelector(".content-feed");
 
 function generatePostsHTML(postsArr) {
   return postsArr.map((upload) => {
-    const { avatar, comment, likes, location, name, post, username } = upload;
+    const { avatar, uuid, isLiked, isCommented, isShared, comment, likes, location, name, post, username } = upload;
     return `
             <article class="post">
               <header class='post-header'>
@@ -18,30 +18,64 @@ function generatePostsHTML(postsArr) {
               </header>
               <img class="post-img" src="${post}" />
               <div class="post-actions">
-                <button aria-label="like">
-                  <i class="fa-regular fa-heart"></i>
-                </button>
-                <button aria-label="comment">
-                  <i class="fa-regular fa-comment"></i>
-                </button>
-                <button aria-label="share">
-                  <i class="fa-regular fa-paper-plane"></i>
-                </button>
-                <span class="like-count">${likes}</span>
+                <div class="post-actions-button-div">
+                    <button aria-label="like">
+                      <i data-like='${uuid}' id='like-btn' class="fa-${isLiked ? 'solid' : 'regular'} fa-heart ${isLiked ? 'liked' : ''}"></i>
+                    </button>
+                    <button aria-label="comment">
+                      <i data-comment='${uuid}' id='share-btn'  class="fa-${isCommented ? 'solid' : 'regular'} fa-comment ${isCommented ? 'commented' : ''}"></i>
+                    </button>
+                    <button aria-label="share">
+                      <i data-share='${uuid}' id='share-btn' class="fa-${isShared ? 'solid' : 'regular'} fa-paper-plane ${isShared ? 'shared' : ''}"></i>
+                    </button>
+                </div>
+                <span class="like-count">${likes} likes</span>
               </div>
-              <div class="user-message-div">
-                <span class="username">${username}</span>
-                <p class="comment">${comment}</p>
+              <div class="message-div">
+                <p class="comment"><span class='username'>${username}</span> ${comment}</p>
               </div>
             </article>
         `;
   }).join('');
 }
+renderFeed();
 
 function renderFeed() {
     const postsHTML = generatePostsHTML(posts)
     contentFeed.innerHTML = postsHTML
 }
 
-renderFeed();
-console.log(renderPostsHTML(posts))
+function findSelectedPost(uuid) {
+  return posts.find(post => post.uuid === uuid)
+}
+
+const handleLikeClick = (uuid) => {
+  const targetPost = findSelectedPost(uuid)
+  targetPost.isLiked = !targetPost.isLiked
+  renderFeed()
+}
+
+const handleShareClick = (uuid) => {
+  const targetPost = findSelectedPost(uuid)
+  targetPost.isShared = !targetPost.isShared
+  renderFeed();
+}
+
+const handleCommentClick = (uuid) => {
+  const targetTweetObj = findSelectedPost(uuid)
+  targetTweetObj.isCommented = !targetTweetObj.isCommented
+  renderFeed()
+}
+
+document.addEventListener('click', (e) => {
+  const t = e.target.dataset
+  if (t.like) {
+    handleLikeClick(t.like)
+  } else if (t.share) {
+    handleShareClick(t.share)
+  } else if (t.comment) {
+    handleCommentClick(t.comment)
+  }
+})
+
+console.log(contentFeed.children)
